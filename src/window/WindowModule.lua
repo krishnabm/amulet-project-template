3e5ec5707d8ed76cc8fb07edc71419e283d2d894
Krishna BM <krishna.bm1995@gmail.com>
2023-06-04 14:01:29 +0530

    verbose configs

--- src/window/WindowModule.lua
+++ src/window/WindowModule.lua
@@ -4,7 +4,11 @@ WindowModule.win = am.window {
   title = "Hello",
   width = 800,
   height = 600,
-  clear_color = vec4(0.5, 0, 0.5, 1)
+  clear_color = vec4(0.5, 0, 0.5, 1),
+  borderless = false,
+  resizable = true,
+  msaa_samples = 2, -- low antialiasing
+  highdpi = false
 }
 WindowModule.win.scene = am.group();
 
