4addc447a63afb33e372e6744f910a0d230123b4
Krishna BM <krishna.bm1995@gmail.com>
2023-06-04 14:04:26 +0530

    hello world changes

--- src/scenes/HelloWorld-scene/HelloWorldScene.lua
+++ src/scenes/HelloWorld-scene/HelloWorldScene.lua
@@ -1,7 +1,8 @@
 -- Node with inline actions
-local rootNode = am.scale(2)
-    ^ am.rotate(math.rad(90)):action(function(node)
-      node.angle = am.frame_time * 0.5
+local rootNode = am.translate(0, 100)
+    ^ am.scale(4)
+    ^ am.rotate(0):action(function(node)
+      node.angle = math.sin(math.rad(am.frame_time * 512)) * 0.02
     end)
     ^ am.text("Hello World!")
 
